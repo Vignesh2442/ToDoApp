5c00d46fb16ba64ddf1bc99120c49a3af77bcb74
James Walker <walker.james39@hotmail.co.uk>
2020-04-01 20:50:02 +0100

    Nav Bar color n buttons

--- App/todoapp/lib/main.dart
+++ App/todoapp/lib/main.dart
@@ -32,47 +32,71 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp(
+    return MaterialApp(
         color: Colors.yellow,
-        home: DefaultTabController(
-          length: 3,
-          child: new Scaffold(
-            body: TabBarView(
-              children: [
-                new Container(
-                  color: Colors.yellow,
+        home: SafeArea(
+          child: DefaultTabController(
+            length: 3,
+            child: new Scaffold(
+              body: Stack(
+                children: <Widget>[
+                  TabBarView(
+                  children: [
+                    new Container(
+                      color: Colors.grey,
+                    ),
+                    new Container(
+                      color: Colors.lightGreen,
+                    ),
+                    new Container(
+                      color: Colors.red,
+                    ),
+                  ],
                 ),
-                new Container(
-                  color: Colors.lightGreen,
+                Container(
+                  height:130,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30)
+                      ),
+                    color: const Color.fromARGB(0XFF,0X33, 0X66, 0X8B),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("In-tray"),
+                      Container()
+                    ],
+                  )
+                )
+                ]
+              ),
+              appBar: AppBar(
+                elevation: 0,
+                title: new TabBar(
+                  tabs: [
+                    Tab(
+                      icon: new Icon(Icons.home),
+                    ),
+                    Tab(
+                      icon: new Icon(Icons.perm_identity),
+                    ),
+                    Tab(
+                      icon: new Icon(Icons.settings)
+                      ,)
+                  ],
+                  labelColor: Colors.yellow,
+                  unselectedLabelColor: Colors.white30,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorPadding: EdgeInsets.all(5.0),
                 ),
-                new Container(
-                  color: Colors.red,
-                ),
-              ],
+                backgroundColor: const Color.fromARGB(0XFF,0X33, 0X66, 0X8B),
+              ),
+              backgroundColor: const Color.fromARGB(0XFF,0X33, 0X66, 0X8B),
             ),
-            appBar: new TabBar(
-              tabs: [
-                Tab(
-                  icon: new Icon(Icons.home),
-                ),
-                Tab(
-                  icon: new Icon(Icons.perm_identity),
-                ),
-                Tab(
-                  icon: new Icon(Icons.settings)
-                  ,)
-              ],
-              labelColor: Colors.yellow,
-              unselectedLabelColor: Colors.blue,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorPadding: EdgeInsets.all(5.0),
-              indicatorColor: Colors.red,
-            ),
-            backgroundColor: Colors.black,
           ),
         ),
-      ),
     );
   }
 }
